bb28a4a2bd6efa4d8c32005792957edd5cfb2b20
ChetanXpro <chetanbaliyan10@gmail.com>
2022-12-24 21:15:07 +0530

    Feat: added new command in slack bot

--- app.js
+++ app.js
@@ -2,12 +2,8 @@ require("dotenv").config();
 const { RTMClient } = require("@slack/rtm-api");
 const { WebClient } = require("@slack/web-api");
 
-const rtm = new RTMClient(
-  process.env.RTM_CLIENT
-);
-const web = new WebClient(
-  process.env.WEB_CLIENT
-);
+const rtm = new RTMClient(process.env.RTM_CLIENT);
+const web = new WebClient(process.env.WEB_CLIENT);
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
@@ -17,13 +13,25 @@ const openai = new OpenAIApi(configuration);
 
 const generateText = async (slackText) => {
   try {
+    if (slackText?.startsWith("#cmd")) {
+      const newPrompt = slackText.replace("#cmd", "").trim();
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `write a command to ${newPrompt}`,
+        temperature: 0,
+
+        max_tokens: 200,
+      });
+
+      return response.data.choices[0].text;
+    }
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: slackText,
       temperature: 0,
       max_tokens: 1000,
     });
-    console.log(response.data.choices[0].text);
+
     return response.data.choices[0].text;
   } catch (error) {
     console.log(error);
@@ -32,21 +40,23 @@ const generateText = async (slackText) => {
 
 rtm.start().catch(console.error);
 
+
+
 rtm.on("ready", async () => {
   console.log("bot started");
-  
 });
 
 rtm.on("slack_event", async (eventType, event) => {
- 
-  if (event?.username !== "Techno bott" && event?.type !== "pong") {
+  console.log(event.channel);
+  if (event?.username !== "Open AI" && event?.type !== "pong") {
     if (event.type !== "user_typing") {
       if (event?.text) console.log(event?.text);
 
       if (event?.text) {
+        rtm.sendTyping(event.channel)
         const response = await generateText(event?.text);
-        if(response && !response.startsWith('#'))  sendMsg("#botspam", response);
-       
+
+        if (response) sendMsg(event.channel, response);
       }
     }
   }
@@ -55,8 +65,11 @@ rtm.on("slack_event", async (eventType, event) => {
 const sendMsg = async (slackChannel, msg) => {
   await web.chat.postMessage({
     channel: slackChannel,
+    username: "Open AI",
     text: msg,
   });
+  // await web.chat.meMessage({
+
+  //   text:msg
+  // })
 };
-
-
