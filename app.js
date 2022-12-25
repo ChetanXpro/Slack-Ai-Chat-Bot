49224adfaaed191ab35ddcedd1a9db9f612828df
ChetanXpro <chetanbaliyan10@gmail.com>
2022-12-25 15:16:47 +0530

    minor fix

--- app.js
+++ app.js
@@ -80,6 +80,13 @@ const sendMsg = async (slackChannel, msg) => {
   // })
 };
 
+app.get('/',(req,res)=>{
+  res.json({
+    success:true,
+    message:"API working"
+  })
+})
+
 
 app.listen(5000,()=>{
   console.log(`Server running on port ${5000}`)
